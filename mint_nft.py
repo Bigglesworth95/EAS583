13d044fa82e32c02eb1df9f3d740ed0fd90ed6c8
Bigglesworth95 <danaraujo1995@gmail.com>
2025-07-12 15:01:32 -0400

    fix

--- mint_nft.py
+++ mint_nft.py
@@ -1,13 +1,14 @@
-rom web3 import Web3
-from web3.middleware import geth_poa_middleware
+from web3 import Web3
+from web3.middleware import ExtraDataToPOAMiddleware
 import json
+import secrets
 
 #Step 1: connect to testnet
-w3 = Web3(Web3.HTTPProvider("https://api-avax-test.network/ext/bc/C/rpc"))#connect
+w3 = Web3(Web3.HTTPProvider("https://api.avax-test.network/ext/bc/C/rpc"))#connect
 assert w3.is_connected(), "Failed to connect"#test connection
 
 #Step 2: Inject Middleware
-w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
 
 #Step 3: Load abi
 with open('NFT.abi', 'r') as opened_file: 
@@ -21,24 +22,21 @@ contract = w3.eth.contract(
 
 #Step 5: Setting up the account
 privateKey = "a79d513de7ecaa28514dbff0d1f5c84acc481cb00c9948d241fca21f4abc03ff"
-account = w3.eth.account.from_key(private_key)
+account = w3.eth.account.from_key(privateKey)
 myAddress = account.address
+print(f"Balance: {w3.from_wei(w3.eth.get_balance(myAddress), 'ether')} AVAX")
 
 #Step 5: Mint the NFT using the "claim" Function
-#increment thru nonces until success = True
 success = False
 attemptCount = 1
+gasPrice = w3.eth.gas_price
 while not success:
-    nonce = attemptCount.to_bytes(32, 'big')
     try: 
-        tokenId = contract.functions.claim(myAddress, nonce).call({'from': myAddress})
-
+        nonce = secrets.token_bytes(32)        
         #build transaction
-        tx = contract.functions.claim(myAddress, nonce).build_transaction({
+        tx = contract.functions.claim(myAddress, nonce).transact({
             'from': myAddress,
-            'nonce': w3.eth.get_transaction_count(myAddress),
-            'gas': 300000,  # didn't know what to put; got this # from reddit
-            'gasPrice': w3.eth.gas_price
+            'gas':3000000
         })
 
         #sign transaction
@@ -48,9 +46,11 @@ while not success:
 
         if receipt.status == 1: 
             success = True
+            print("Minted: ", nonce)
             break
-        else: 
-            nonce+=1
+       
     except Exception as e: 
+        print(attemptCount)
+        attemptCount+=1
         continue
 
