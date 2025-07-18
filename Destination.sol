a9ee8ae43da804cb5e85b0ee72737e4b1a5d2c34
Bigglesworth95 <danaraujo1995@gmail.com>
2025-07-18 14:37:18 -0400

    initi commit

---
+++ Destination.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./BridgeToken.sol";
+
+contract Destination is AccessControl {
+    bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
+    bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
+	mapping( address => address) public underlying_tokens;
+	mapping( address => address) public wrapped_tokens;
+	address[] public tokens;
+
+	event Creation( address indexed underlying_token, address indexed wrapped_token );
+	event Wrap( address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount );
+	event Unwrap( address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount );
+
+    constructor( address admin ) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(CREATOR_ROLE, admin);
+        _grantRole(WARDEN_ROLE, admin);
+    }
+
+	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
+		//YOUR CODE HERE
+
+		require(underlying_tokens[_unerlying_token] != address(0));
+		wrappedToken = wrapped_tokens[_underlying_token];
+		mint(address_recipiet, _amount);
+		BridgeToken wrapped_token.mint(address_recipiet, _amount);
+		emit Wrap(_underlying_token, wrappedToken, _recipient, _amount);
+	}
+
+	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
+		require(wrapped_tokens[_wrapped_token] != address(0))
+		
+		address underlyingToken = wrapped_tokens[_wrapped_token];
+		BridgeToken(_wrapped_token).burnFrom(_recipient, _amount);
+		emit Unwrapped(msg.sender, _amount) 
+		
+	}
+
+	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
+		//YOUR CODE 
+		
+		BridgeToken token = new BridgeToken(_underlying_token, name, symbol);
+		address tokenAddress = address(token);
+
+		underlying_tokens[_unerlying_token] = wrappedAddress;
+		wrapped_tokens[wrappedAddress] = _underlying_token;
+		tokens.push(wrappedAddress);
+
+		emit Creation(tokenAddress, name, symbol);
+		return tokenAddress;
+	}
+
+}
+
+
