a66e4a4ffaefe5a591b0973971e9197017987f32
Bigglesworth95 <danaraujo1995@gmail.com>
2025-07-19 14:39:47 -0400

    fix

--- BridgeToken.sol
+++ BridgeToken.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "forge-std/console2.sol"; 
 
 contract BridgeToken is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 	address public underlying;
 
     constructor( address _underlying, string memory name, string memory symbol, address admin ) ERC20(name,symbol) {
-		console2.log("BridgeToken constructor called!");
         underlying = _underlying;
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(MINTER_ROLE, admin);
--- Destination.sol
+++ Destination.sol
@@ -48,43 +48,18 @@ contract Destination is AccessControl {
         emit Unwrap(underlyingToken, _wrapped_token, msg.sender, _recipient, _amount);
     }
 
-	function createToken(
-		address _underlying_token,
-		string memory name,
-		string memory symbol
-	) public onlyRole(CREATOR_ROLE) returns (address) {
-		console2.log("createToken called");
-		console2.log("  msg.sender:", msg.sender);
-		console2.log("  has CREATOR_ROLE?", hasRole(CREATOR_ROLE, msg.sender));
+    function createToken(address _underlying_token, string memory name, string memory symbol) public onlyRole(CREATOR_ROLE) returns(address) {
+        
+		
+		require(_underlying_token != address(0), "Invalid underlying");
+        
+        BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this));
+        address tokenAddress = address(newToken);
 
-		address currentlyWrapped = wrapped_tokens[_underlying_token];
-		console2.log("  wrapped_tokens[underlying] before:", currentlyWrapped);
-
-		require(currentlyWrapped == address(0), "Token already registered");
-		console2.log("  require passed, token not yet registered");
-
-		console2.log("  deploying BridgeToken for underlying:", _underlying_token);
-		console2.log("  name/symbol:", name, symbol);
-
-		BridgeToken newToken = new BridgeToken(
-			_underlying_token,
-			name,
-			symbol,
-			address(this)
-		);
-		address tokenAddress = address(newToken);
-		console2.log("  deployed new BridgeToken at:", tokenAddress);
-
-		underlying_tokens[tokenAddress]   = _underlying_token;
-		wrapped_tokens[_underlying_token] = tokenAddress;
-		tokens.push(tokenAddress);
-		console2.log("  underlying_tokens[wrapped] =", underlying_tokens[tokenAddress]);
-		console2.log("  wrapped_tokens[underlying] =", wrapped_tokens[_underlying_token]);
-		console2.log("  tokens array length now:", tokens.length);
-
-		emit Creation(_underlying_token, address(0));
-		console2.log("createToken returning:", tokenAddress);
+        underlying_tokens[tokenAddress] = _underlying_token;
+        wrapped_tokens[_underlying_token] = tokenAddress;
+        tokens.push(tokenAddress);
+		emit Creation(_underlying_token, tokenAddress);
 		return tokenAddress;
-	}
-
+    }
 }
