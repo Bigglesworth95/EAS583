caa8642d2ca7fa298ee9275227560cfae6ccc9d8
Bigglesworth95 <danaraujo1995@gmail.com>
2025-07-25 14:17:02 -0400

    fixed

--- Source.sol
+++ Source.sol
@@ -23,28 +23,26 @@ contract Source is AccessControl {
 
 	function deposit(address _token, address _recipient, uint256 _amount ) public {
 		//Check if token being depoisted has been registered
-		require(approved)[token];
+		requirerequire(approved[_token]);
 		//Use transferFrom to pull the tokens into the deposit contract
-		ERC20(token).transferFrom(msg.sender, address(this), amount);
+		ERC20(_token).transferFrom(msg.sender, address(this), _amount);
 		//Emit a deposit event so that the bridge operator knows to make the necessary actions
 		//on the destination side
-		emit Depoist(token, msg.sender, amount);
+		emit Deposit(_token, msg._recipient, _amount);
 	}
 
 	function withdraw(address _token, address _recipient, uint256 _amount ) onlyRole(WARDEN_ROLE) public {
 		//check if function is being called by the contract owner
-		require(msg.sender ==owner);
-
+		require(msg.sender == _recipient);
 		//push the tokens to the recipient using the ERC20 transfer function
-		ERC20(token).transfer(_recipient, _amount);
+		ERC20(_token).transfer(_recipient, _amount);
 
 		//emit a withdraw event
-		emit Withdrawal(token, recipient, amount);
+		emit Withdrawal(token, _recipient, _amount);
 	}
 
 	function registerToken(address _token) onlyRole(ADMIN_ROLE) public {
 		//check that function is being called by the contract owner
-		require(msg.sender == owner);
 
 		//check that token has not already been registered
 		require(!approved[_token]);
@@ -53,7 +51,7 @@ contract Source is AccessControl {
 		approved[_token] = true;
 
 		//emit registration event
-		emit Withdrawal(_token, _recipient, _amount);
+		emit Registration(_token);
 
 
 	}
