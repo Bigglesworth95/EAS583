0472004239730992fbb8d28cf7a194d3b3923f7c
Bigglesworth95 <danaraujo1995@gmail.com>
2025-08-05 14:30:47 -0400

    init commit

---
+++ bridge.py
@@ -0,0 +1,102 @@
+from web3 import Web3
+from web3.providers.rpc import HTTPProvider
+from web3.middleware import ExtraDataToPOAMiddleware #Necessary for POA chains
+from datetime import datetime
+import json
+import pandas as pd
+
+
+def connect_to(chain):
+    if chain == 'source':  # The source contract chain is avax
+        api_url = f"https://api.avax-test.network/ext/bc/C/rpc" #AVAX C-chain testnet
+
+    if chain == 'destination':  # The destination contract chain is bsc
+        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/" #BSC testnet
+
+    if chain in ['source','destination']:
+        w3 = Web3(Web3.HTTPProvider(api_url))
+        # inject the poa compatibility middleware to the innermost layer
+        w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
+    return w3
+
+
+def get_contract_info(chain, contract_info):
+    """
+        Load the contract_info file into a dictionary
+        This function is used by the autograder and will likely be useful to you
+    """
+    try:
+        with open(contract_info, 'r')  as f:
+            contracts = json.load(f)
+    except Exception as e:
+        print( f"Failed to read contract info\nPlease contact your instructor\n{e}" )
+        return 0
+    return contracts[chain]
+
+
+
+def scan_blocks(chain, contract_info="contract_info.json"):
+    """
+        chain - (string) should be either "source" or "destination"
+        Scan the last 5 blocks of the source and destination chains
+        Look for 'Deposit' events on the source chain and 'Unwrap' events on the destination chain
+        When Deposit events are found on the source chain, call the 'wrap' function the destination chain
+        When Unwrap events are found on the destination chain, call the 'withdraw' function on the source chain
+    """
+
+    # This is different from Bridge IV where chain was "avax" or "bsc"
+    if chain not in ['source','destination']:
+        print( f"Invalid chain: {chain}" )
+        return 0
+        #YOUR CODE HERE
+
+    #connect to the chain
+    w3 = connect_to(chain)
+
+    #get the abi & address
+    contractInfo = get_contract_info(chain, contract_info)
+
+    #get the contract
+    contract = w3.eth.contract(abi = contractInfo["abi"], address = contractInfo["address"])
+
+    #get accounts
+    privateKey = "a79d513de7ecaa28514dbff0d1f5c84acc481cb00c9948d241fca21f4abc03ff"
+    account = w3.eth.account.from_key(privateKey)
+    accountAddress = account.address
+    nonce = w3.eth.get_transaction_count(accountAddress)
+
+    #scan
+    counter = 0
+    #loop thru and get the info from each block
+    while counter <= 4: 
+        cur_block = w3.eth.block_number - counter
+        if chain == "source":
+            deposits = contract.events.Deposit().get_logs(fromBlock=cur_block, toBlock=cur_block)
+            for deposit in deposits:
+                tokenAddress = deposit["args"]["token"]
+                recipient = deposit["args"]["recipient"]
+                amount = deposit["args"]["amount"]
+                contract.wrap(tokenAddress, recipient, amount).build_transaction({
+                    "from": accountAddress, 
+                    "nonce": nonce,
+                    "gas": 30000, 
+                    "gasPrice": w3.eth.gas_price,
+                    "chainId": 97
+                })
+                nonce+=1
+        else:
+            unwraps = contract.events.Unwrap().get_logs(fromBlock=cur_block, toBlock = cur_block)
+            for unwrap in unwraps:
+                tokenAddress = unwrap["args"]["token"]
+                recipient = unwrap["args"]["recipient"]
+                amount = unwrap["args"]["amount"]
+                contract.withdraw(tokenAddress, recipient, amount).build_transaction({
+                    "from": accountAddress,
+                    "nonce": nonce,
+                    "gas": 30000,
+                    "gasPrice": w3.eth.gas_price,
+                    "chainId": 43113
+                })
+                nonce+=1
+        counter +=1
+
---
+++ erc20Setup.py
@@ -0,0 +1,87 @@
+from web3 import Web3
+from web3.providers.rpc import HTTPProvider
+import csv
+import json
+
+try: 
+    with open("erc20s.csv", "r") as f: 
+        lines = f.readlines()
+except Exception as e: 
+    print(e)
+
+tokens = []
+for line in lines: 
+    line = line.split(",")
+    if line[0] == "chain":
+        continue
+    else:
+        chain = line[0].strip()
+        address = line[1].strip()
+    
+
+    
+    tokens.append((chain, address))
+
+private_key = "a79d513de7ecaa28514dbff0d1f5c84acc481cb00c9948d241fca21f4abc03ff"
+
+#connect to testnet RPC
+avax_rpc = "https://api.avax-test.network/ext/bc/C/rpc"
+bnb_rpc = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+avax_w3 = Web3(HTTPProvider(avax_rpc))
+bnb_w3 = Web3(HTTPProvider(bnb_rpc))
+
+
+#get abi
+with open("contract_info.json") as f:
+    info = json.load(f)
+
+#get contract info
+source_abi = info["source"]["abi"]
+source_addr = info["source"]["address"]
+
+dest_abi = info["destination"]["abi"]
+dest_addr = info["destination"]["address"]
+
+#get contracts
+srcContract = avax_w3.eth.contract(abi = source_abi, address = source_addr)
+destContract = bnb_w3.eth.contract(abi=dest_abi, address=dest_addr)
+
+#get account
+avax_acct = avax_w3.eth.account.from_key(private_key)
+avaxAddress = avax_acct.address
+bnb_account = bnb_w3.eth.account.from_key(private_key)
+bnbAddress = bnb_account.address
+avaxNonce = avax_w3.eth.get_transaction_count(avaxAddress)
+bnbNonce = bnb_w3.eth.get_transaction_count(bnbAddress)
+for token in tokens:
+    num = 1 
+    tokenAddress = token[1]
+    if token[0] == "avax":
+        tx = srcContract.functions.registerToken(tokenAddress).build_transaction({
+            'from': avaxAddress,
+            'nonce': avaxNonce,
+            'gas': 30000,
+            'gasPrice': avax_w3.eth.gas_price,
+            'chainId': 43113
+        })
+
+        signedTx = avax_w3.eth.account.sign_transaction(tx, private_key)
+        txHash = avax_w3.eth.send_raw_transaction(signedTx.raw_transaction)
+        receipt = avax_w3.eth.wait_for_transaction_receipt(txHash)
+        avaxNonce +=1
+
+    else:
+        tx = destContract.functions.createToken(tokenAddress, f"bridgeToken{num}", "symbol{num}").build_transaction({
+            "from": bnbAddress, 
+            "nonce": bnbNonce, 
+            "gas": 30000, 
+            "gasPrice": bnb_w3.eth.gas_price,
+            "chainId": 97
+        })
+
+        signedTx = bnb_w3.eth.account.sign_transaction(tx, private_key)
+        txHash = bnb_w3.eth.send_raw_transaction(signedTx.raw_transaction)
+        receipt = bnb_w3.eth.wait_for_transaction_receipt(txHash)
+        bnbNonce +=1
+    num+=1
+
