ce952cc9f9108b828ff253215f02f2717b434954
Bigglesworth95 <danaraujo1995@gmail.com>
2025-08-06 11:45:22 -0400

    fix

--- bridge.py
+++ bridge.py
@@ -51,61 +51,69 @@ def scan_blocks(chain, contract_info="contract_info.json"):
         return 0
         #YOUR CODE HERE
 
-    #connect to the chain
-    w3 = connect_to(chain)
+    #connect to the chains
+    listenerW3 = connect_to(chain)
+    activeChain = "source" if chain == "destination" else "destination"
+    activeW3 = connect_to(activeChain)
 
-    #get the abi & address
-    contractInfo = get_contract_info(chain, contract_info)
-
+    #get the abi & address info
+    listenerContractInfo = get_contract_info(chain, contract_info)
+    activeContractInfo = get_contract_info(activeChain, contract_info)
     #get the contract
-    contract = w3.eth.contract(abi = contractInfo["abi"], address = contractInfo["address"])
+    listenerContract = listenerW3.eth.contract(abi = listenerContractInfo["abi"], address = listenerContractInfo["address"])
+    activeContract = activeW3.eth.contract(abi = activeContractInfo["abi"], address = activeContractInfo["address"])
 
     #get accounts
     privateKey = "a79d513de7ecaa28514dbff0d1f5c84acc481cb00c9948d241fca21f4abc03ff"
-    account = w3.eth.account.from_key(privateKey)
-    accountAddress = account.address
-    nonce = w3.eth.get_transaction_count(accountAddress)
+    listenerAccount = listenerW3.eth.account.from_key(privateKey)
+    listenerAccountAddress = listenerAccount.address
+    listenerNonce = listenerW3.eth.get_transaction_count(listenerAccountAddress)
+
+    activeAccount = activeW3.eth.account.from_key(privateKey)
+    activeAccountAddress = activeAccount.address
+    activeNonce = activeW3.eth.get_transaction_count(activeAccountAddress)
+
 
     #scan
     counter = 0
     #loop thru and get the info from each block
     while counter <= 4: 
-        cur_block = w3.eth.block_number - counter
+        cur_block = listenerW3.eth.block_number - counter
         if chain == "source":
-            depositFilter = contract.events.Deposit().create_filter(from_block=cur_block, to_block=cur_block)
+            depositFilter = listenerContract.events.Deposit.create_filter(from_block=cur_block, to_block=cur_block)
             deposits = depositFilter.get_all_entries()
             for deposit in deposits:
                 tokenAddress = deposit["args"]["token"]
                 recipient = deposit["args"]["recipient"]
                 amount = deposit["args"]["amount"]
-                tx = contract.functions.wrap(tokenAddress, recipient, amount).build_transaction({
-                    "from": accountAddress, 
+                tx = activeContract.functions.wrap(tokenAddress, recipient, amount).build_transaction({
+                    "from": activeAccountAddress, 
                     "nonce": nonce,
                     "gas": 30000, 
-                    "gasPrice": w3.eth.gas_price,
+                    "gasPrice": activeW3.eth.gas_price,
                     "chainId": 43113
                 })
                 nonce+=1
-                signedTx = w3.eth.account.sing_transaction(tx, privateKey)
-                txHash = w3.eth.send_raw_transaction(signedTx.raw_transaction)
-                receipt = w3.eth.wait_for_transaction_receipt(txHash)
+                signedTx = activeW3.eth.account.sign_transaction(tx, privateKey)
+                txHash = activeW3.eth.send_raw_transaction(signedTx.raw_transaction)
+                receipt = activeW3.eth.wait_for_transaction_receipt(txHash)
         else:
-            unwrapFilter = contract.events.Unwrap().create_filter(from_block=cur_block, to_block = cur_block)
+            unwrapFilter = listenerContract.events.Unwrap.create_filter(from_block=cur_block, to_block = cur_block)
             unwraps = unwrapFilter.get_all_entries()
             for unwrap in unwraps:
                 tokenAddress = unwrap["args"]["token"]
                 recipient = unwrap["args"]["recipient"]
                 amount = unwrap["args"]["amount"]
-                tx = contract.functions.withdraw(tokenAddress, recipient, amount).build_transaction({
-                    "from": accountAddress,
+                tx = activeContract.functions.withdraw(tokenAddress, recipient, amount).build_transaction({
+                    "from": activeAccountAddress,
                     "nonce": nonce,
                     "gas": 30000,
-                    "gasPrice": w3.eth.gas_price,
+                    "gasPrice": activeW3.eth.gas_price,
                     "chainId": 97
                 })
                 nonce+=1
-                signedTx = w3.eth.account.sing_transaction(tx, privateKey)
-                txHash = w3.eth.send_raw_transaction(signedTx.raw_transaction)
-                receipt = w3.eth.wait_for_transaction_receipt(txHash)
+                signedTx = activeW3.eth.account.sign_transaction(tx, privateKey)
+                txHash = activeW3.eth.send_raw_transaction(signedTx.raw_transaction)
+                receipt = activeW3.eth.wait_for_transaction_receipt(txHash)
         counter +=1
 
